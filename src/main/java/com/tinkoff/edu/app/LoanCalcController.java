9c3143af0333e4dfcf5d55b9bb5387bdb74f3ed4
n.lyalin <n.lyalin@tinkoff.ru>
2021-08-16 02:31:38 +0300

    Add POM.xml (homework №3) Create LoanCalc app + proto test

---
+++ src/main/java/com/tinkoff/edu/app/LoanCalcController.java
@@ -0,0 +1,10 @@
+package com.tinkoff.edu.app;
+
+public class LoanCalcController {
+    /*
+    Validates and logs requests
+    */
+    public static int createRequest () {
+        return LoanCalcService.createRequest();
+    }
+}
